f15058d4c406057904cf513d5fccb4e7100545ba
Norio Nomura <norio.nomura@gmail.com>
2012-07-21 11:36:18 +0900

    Add #error to notify about API keys

--- ReadabilityForEchofon/ReadabilityAPI_KEY.m
+++ ReadabilityForEchofon/ReadabilityAPI_KEY.m
@@ -7,5 +7,6 @@
 
 NSString * const kReadabilityAPI_KEY = @"";
 NSString * const kReadabilityAPI_SECRET = @"";
+#error You need get API key and secret from http://www.readability.com/developers/api , then comment out this line.
 NSString * const kReadabilityBASE = @"https://www.readability.com/";
 NSString * const kReadabilityAPI_CALLBACK_URL = @"http://norio-nomura.github.com/SimblPluginsForEchofon/ReadabilityForEchofonCallback.html";
